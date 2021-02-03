e1e7f0eee93e2fddb2672d855a63416cc05136e3
Your Name <emanuele.campanelli@studio.unibo.it>
2021-02-04 00:21:02 +0100

    .

---
+++ first-year-of-University/logica per l'informatica/haskell/ricorsione strutturale/esercitazioni.hs
@@ -0,0 +1,32 @@
+-- Es1 - ESPRESSIONI ARITMETICHE
+
+    {-
+    Consegna esercizio: da scrivere
+
+    -}
+    data E = Var String | Num Int | Plus E E | Mult E E deriving Show
+    
+
+------------------------------------------------------------------------------------
+
+-- Es2
+
+    {-
+    Consegna esercizio: da scrivere
+
+    -}
+
+
+
+
+------------------------------------------------------------------------------------
+
+-- Es3
+
+    {-
+    Consegna esercizio: da scrivere
+
+    -}
+
+
+
