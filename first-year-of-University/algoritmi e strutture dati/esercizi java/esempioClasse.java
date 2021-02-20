0b17945ef7776355733f20147e4bf50535ba368f
Your Name <emanuele.campanelli@studio.unibo.it>
2021-02-20 13:17:35 +0100

    .

---
+++ first-year-of-University/algoritmi e strutture dati/esercizi java/esempioClasse.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Es4 {
+
+    public class Contatore {
+            protected int counter;
+            Contatore(){ counter = 0; }
+            public int valoreContatore(){ return counter; }
+            public void incrementaContatore() { counter++; }
+            public void decrementaContatore() { counter--; }
+    }
+}
