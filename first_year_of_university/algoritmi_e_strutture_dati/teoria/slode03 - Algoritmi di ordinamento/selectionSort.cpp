6d8121452a433669ac7749a077c7dbc1d35f2f2a
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2021-04-12 17:11:05 +0200

    .

--- first_year_of_university/algoritmi_e_strutture_dati/teoria/slode03 - Algoritmi di ordinamento/selectionSort.cpp
+++ first_year_of_university/algoritmi_e_strutture_dati/teoria/slode03 - Algoritmi di ordinamento/selectionSort.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-int* createArray () {
-  int* array = 0; int length = 0;
-  cout << "Array length: "; cin >> length;
-
-  for (int pos = 0; pos < length; pos++) {
-    cin >> array[pos];
+void fillEmptyArray (int in_array[], int in_length) {
+  cout << "Inserire " << in_length << " elementi:" << endl;
+  for (int pos = 0; pos < in_length; pos++) {
+    cin >> in_array[pos];
   }
-  return array;
+}
+
+void printArray (int in_array[], int in_length) {
+  for (int pos = 0; pos < in_length; pos++) {
+    cout << in_array[pos] << " ";
+  }
 }
 
 int main () {
 
+  int length = 0;
+  cout << "Lunghezza dell'array: "; cin >> length;
 
-
-  int* array = createArray ();
-
+  int array[length];
+  fillEmptyArray (array, length);
 
   return 0;
 
