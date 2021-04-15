15c9ebd45183dd12f91a649e4ee7379da41ec56f
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2021-04-16 01:41:06 +0200

    .

--- first_year_of_university/algebra_e_geometria/capitolo2/VectorSpace.hpp
+++ first_year_of_university/algebra_e_geometria/capitolo2/VectorSpace.hpp
@@ -1,3 +1,27 @@
 #include <iostream>
 
 using namespace std;
+
+class VectorSpace {
+private:
+
+protected:
+    int n;
+    int point[];
+public:
+    VectorSpace (int in_n) {
+        this -> n = in_n;
+        this -> point[in_n];
+    }
+
+    int* creatwPoint () {
+        
+    }
+
+    bool closedSum () {
+        int point1[this -> n];
+        for (int pos = 0; pos < this -> n; pos++) point1
+        this -> point;
+    }
+
+};
