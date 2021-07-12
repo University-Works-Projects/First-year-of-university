3e9f119a41843d57ca096ed4a46760215e4d445b
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2021-07-12 15:11:19 +0200

    .

---
+++ first_year_of_university/logica_per_l_informatica/3_CFU/esami&simulazioni/2021_02_17.hs
@@ -0,0 +1,22 @@
+-- Es 1 - Matematica
+-- Su Notion: https://www.notion.so/Logica-3-CFU-cd84473e2d2540e2b10af62529668ceb
+
+
+-- Es 2 - Informatica - Da finire di scrivere
+
+    {- Consegna
+        Una lista L di naturali è definita dalla seguente grammatica:
+            L := [] | N :: L
+        Esempio:
+            3 :: 4 :: 5 :: []
+
+        Un albero binatio di naturali è definito dalla seguente grammatica:
+            T := Nil | T # N # T
+        Esempio:
+            (Nil # 3 # Nil) # 4 # ( Nil # 5 # Nil)
+            È l'albero che ha 4 come radice e si ramifica in un sottoalbero si sinistra che contiene 3 e due rami vuoti
+            e uno di destra dalla seguente grammatica:
+        -}
+
+-- Es 3 - Matematica
+-- Su Notion: https://www.notion.so/Logica-3-CFU-cd84473e2d2540e2b10af62529668ceb
